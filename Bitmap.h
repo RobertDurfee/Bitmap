35d4b8f9a2395cd76e6fb31ec2f4ac71a6d5e968
Robert Durfee <rbdurfee@users.noreply.github.com>
2017-02-10 13:30:38 -0600

    Update Bitmap.h

--- Bitmap.h
+++ Bitmap.h
@@ -26,7 +26,8 @@ struct BITMAP_IMAGE_HEADER
 	unsigned long Importantpixels;
 };
 
-#define SIZE_OF_HEADERS 0x36
+#define SIZE_OF_HEADERS		0x36
+#define PADDING_CONSTANT	0xCC
 
 class Bitmap
 {
@@ -36,6 +37,8 @@ public:
 	Bitmap(int width, int height, unsigned char * pixels);
 	Bitmap(int width, int height);
 	Bitmap(char * filename);
+
+	~Bitmap();
 
 	void Open(char * filename);
 	void Save(char * filename);
@@ -57,50 +60,57 @@ private:
 };
 Bitmap::Bitmap(BITMAP_FILE_HEADER fileHeader, BITMAP_IMAGE_HEADER imageHeader)
 {
-	this->SetHeaders(fileHeader, imageHeader);
+	SetHeaders(fileHeader, imageHeader);
 	
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
+	PixelData = (unsigned char *)malloc(ImageHeader.ImageSize);
 }
 Bitmap::Bitmap(BITMAP_FILE_HEADER fileHeader, BITMAP_IMAGE_HEADER imageHeader, unsigned char * pixels)
 {
-	this->SetHeaders(fileHeader, imageHeader);
+	SetHeaders(fileHeader, imageHeader);
 	
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
+	PixelData = (unsigned char *)malloc(ImageHeader.ImageSize);
 
-	this->SetPixels(pixels);
+	SetPixels(pixels);
 }
 Bitmap::Bitmap(int width, int height, unsigned char * pixels)
 {
-	this->SetHeaders(width, height);
+	SetHeaders(width, height);
 
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
+	PixelData = (unsigned char *)malloc(ImageHeader.ImageSize);
 
-	this->SetPixels(pixels);
+	SetPixels(pixels);
 }
 Bitmap::Bitmap(int width, int height)
 {
-	this->SetHeaders(width, height);
+	SetHeaders(width, height);
 
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
+	PixelData = (unsigned char *)malloc(ImageHeader.ImageSize);
 }
 Bitmap::Bitmap(char * filename)
 {
-	this->Open(filename);
+	Open(filename);
+}
+
+Bitmap::~Bitmap()
+{
+	free(PixelData);
 }
 
 void Bitmap::Open(char * filename)
-{
-	if (this->PixelData)
-		free(this->PixelData);
-	
+{	
 	std::ifstream file(filename, std::ios::binary);
 
-	char headers[SIZE_OF_HEADERS];
+	char * headers = (char *)malloc(SIZE_OF_HEADERS);
 	file.read(headers, SIZE_OF_HEADERS);
-	this->SetHeaders(headers);
+	SetHeaders(headers);
+	free(headers);
 
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
-	file.read((char *)this->PixelData, this->ImageHeader.ImageSize);
+	if (PixelData)
+		realloc(PixelData, ImageHeader.ImageSize);
+	else
+		PixelData = (char *)malloc(ImageHeader.ImageSize);
+
+	file.read((char *)PixelData, ImageHeader.ImageSize);
 
 	file.close();
 }
@@ -108,119 +118,108 @@ void Bitmap::Save(char * filename)
 {
 	std::ofstream file(filename, std::ios::binary);
 
-	char * headers = this->GetHeaders();
+	char * headers = GetHeaders();
 	file.write(headers, SIZE_OF_HEADERS);
 	free(headers);
 
-	file.write((char *)this->PixelData, this->ImageHeader.ImageSize);
+	file.write((char *)PixelData, ImageHeader.ImageSize);
 
 	file.close();
 }
 
 unsigned char * Bitmap::GetPixels(int * width, int * height)
 {
-	*width = this->ImageHeader.Width;
-	*height = this->ImageHeader.Height;
+	*width = ImageHeader.Width;
+	*height = ImageHeader.Height;
 
-	unsigned char * pixels = (unsigned char *)malloc(this->ImageHeader.Width * this->ImageHeader.Height * 3);
+	unsigned char * pixels = (unsigned char *)malloc(ImageHeader.Width * ImageHeader.Height * 3);
 
-	for (int y = 0; y < *height; y++)
-		for (int x = 0; x < *width; x++)
-			for (int i = 0; i < 3; i++)
-				pixels[y * (*width * 3) + (x * 3) + i] = this->PixelData[y * (*width * 3 + this->Padding) + (x * 3) + i];
+	for (int y = 0; y < *height; y++) //Each Row
+		for (int x = 0; x < *width; x++) //Each Column
+			for (int i = 0; i < 3; i++) //Each RGB
+				pixels[y * (*width * 3) + (x * 3) + i] /*Doesn't have padding*/ = PixelData[y * (*width * 3 + Padding) + (x * 3) + i] /*Has padding*/;
 
 	return pixels;
 }
 void Bitmap::SetPixels(int width, int height, unsigned char * pixels)
 {
-	this->ImageHeader.Width = width;
-	this->ImageHeader.Height = height;
-
-	this->Padding = 4 - ((this->ImageHeader.Width * 3) % 4);
-	if (this->Padding == 4)
-		this->Padding = 0;
-
-	this->ImageHeader.ImageSize = (width * height * 3) + this->Padding * height;
-
-	this->FileHeader.Size = this->ImageHeader.ImageSize + 54;
-
-	if (this->PixelData)
-		free(this->PixelData);
-
-	this->PixelData = (unsigned char *)malloc(this->ImageHeader.ImageSize);
-
-	for (int y = 0; y < height; y++)
+	//Update headers if dimensions changed
+	if (width != ImageHeader.Width || height != ImageHeader.Height)
 	{
-		for (int x = 0; x < width; x++)
-			for (int i = 0; i < 3; i++)
-				this->PixelData[y * (width * 3 + this->Padding) + (x * 3) + i] = pixels[y * (width * 3) + (x * 3) + i];
+		SetHeaders(width, height);
+		realloc(PixelData, ImageHeader.ImageSize);
+	}
 
-		for (int i = 0; i < this->Padding; i++)
-			this->PixelData[y * (width * 3 + this->Padding) + (width * 3) + i] = 0xCC;
+	for (int y = 0; y < height; y++) //Each Row
+	{
+		for (int x = 0; x < width; x++) //Each Column
+			for (int i = 0; i < 3; i++) //Each RGB
+				PixelData[y * (width * 3 + Padding) + (x * 3) + i] /*Has padding*/ = pixels[y * (width * 3) + (x * 3) + i] /*Doesn't have padding*/;
+
+		for (int i = 0; i < Padding; i++) //Set Padding
+			PixelData[y * (width * 3 + Padding) + (width * 3) + i] = PADDING_CONSTANT;
 	}
 }
 void Bitmap::SetPixels(unsigned char * pixels)
 {
-	this->SetPixels(this->ImageHeader.Width, this->ImageHeader.Height, pixels);
+	SetPixels(ImageHeader.Width, ImageHeader.Height, pixels);
 }
 
 void Bitmap::SetHeaders(int width, int height)
 {
-	this->Padding = (4 - ((width * 3) % 4));
-	if (this->Padding == 4)
-		this->Padding = 0;
+	if ((Padding = (4 - ((width * 3) % 4))) == 4)
+		Padding = 0; //Padding of 4 is illegal
 
-	this->FileHeader.Type = 0x4D42;
-	this->FileHeader.Reserved = 0x00000000;
-	this->FileHeader.Offset = 54;
+	FileHeader.Type = 0x4D42; //"BM"
+	FileHeader.Reserved = 0x00000000;
+	FileHeader.Offset = 54;
 
-	this->ImageHeader.Size = 40;
-	this->ImageHeader.Width = width;
-	this->ImageHeader.Height = height;
-	this->ImageHeader.Planes = 1;
-	this->ImageHeader.BitCount = 24;
-	this->ImageHeader.Compression = 0;
-	this->ImageHeader.ImageSize = (width * height * 3) + this->Padding * height;
-	this->ImageHeader.WidthPixelsPerMeter = 0;
-	this->ImageHeader.HeightPixelsPerMeter = 0;
-	this->ImageHeader.pixelsUsed = 0;
-	this->ImageHeader.Importantpixels = 0;
+	ImageHeader.Size = 40;
+	ImageHeader.Width = width;
+	ImageHeader.Height = height;
+	ImageHeader.Planes = 1;
+	ImageHeader.BitCount = 24;
+	ImageHeader.Compression = 0;
+	ImageHeader.ImageSize = (width * height * 3) + Padding * height;
+	ImageHeader.WidthPixelsPerMeter = 0;
+	ImageHeader.HeightPixelsPerMeter = 0;
+	ImageHeader.pixelsUsed = 0;
+	ImageHeader.Importantpixels = 0;
 
-	this->FileHeader.Size = this->ImageHeader.ImageSize + 54;
+	FileHeader.Size = ImageHeader.ImageSize + 54;
 }
 void Bitmap::SetHeaders(BITMAP_FILE_HEADER fileHeader, BITMAP_IMAGE_HEADER imageHeader)
 {
-	this->FileHeader = fileHeader;
-	this->ImageHeader = imageHeader;
+	FileHeader = fileHeader;
+	ImageHeader = imageHeader;
 
-	this->Padding = (4 - ((this->ImageHeader.Width * 3) % 4));
-	if (this->Padding == 4)
-		this->Padding = 0;
+	if ((Padding = (4 - ((ImageHeader.Width * 3) % 4))) == 4)
+		Padding = 0; //Padding of 4 is illegal
 }
 void Bitmap::SetHeaders(char * headers)
 {
 	int index = 0;
 
-	this->FileHeader.Type = *(unsigned short*)&headers[index]; index += sizeof(unsigned short);
-	this->FileHeader.Size = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->FileHeader.Reserved = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->FileHeader.Offset = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
+	//memcpy() is not used due to compiler-specfic structure padding
+	FileHeader.Type = *(unsigned short*)&headers[index];			index += sizeof(unsigned short);
+	FileHeader.Size = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
+	FileHeader.Reserved = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
+	FileHeader.Offset = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
 
-	this->ImageHeader.Size = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.Width = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.Height = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.Planes = *(unsigned short*)&headers[index]; index += sizeof(unsigned short);
-	this->ImageHeader.BitCount = *(unsigned short*)&headers[index]; index += sizeof(unsigned short);
-	this->ImageHeader.Compression = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.ImageSize = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.WidthPixelsPerMeter = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.HeightPixelsPerMeter = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.pixelsUsed = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
-	this->ImageHeader.Importantpixels = *(unsigned long*)&headers[index]; index += sizeof(unsigned long);
+	ImageHeader.Size = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
+	ImageHeader.Width = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
+	ImageHeader.Height = *(unsigned long*)&headers[index];			index += sizeof(unsigned long);
+	ImageHeader.Planes = *(unsigned short*)&headers[index];			index += sizeof(unsigned short);
+	ImageHeader.BitCount = *(unsigned short*)&headers[index];		index += sizeof(unsigned short);
+	ImageHeader.Compression = *(unsigned long*)&headers[index];		index += sizeof(unsigned long);
+	ImageHeader.ImageSize = *(unsigned long*)&headers[index];		index += sizeof(unsigned long);
+	ImageHeader.WidthPixelsPerMeter = *(unsigned long*)&headers[index];	index += sizeof(unsigned long);
+	ImageHeader.HeightPixelsPerMeter = *(unsigned long*)&headers[index];	index += sizeof(unsigned long);
+	ImageHeader.pixelsUsed = *(unsigned long*)&headers[index];		index += sizeof(unsigned long);
+	ImageHeader.Importantpixels = *(unsigned long*)&headers[index];
 
-	this->Padding = (4 - ((this->ImageHeader.Width * 3) % 4));
-	if (this->Padding == 4)
-		this->Padding = 0;
+	if ((Padding = (4 - ((ImageHeader.Width * 3) % 4))) == 4)
+		Padding = 0; //Padding of 4 is illegal
 }
 char * Bitmap::GetHeaders()
 {
@@ -228,23 +227,24 @@ char * Bitmap::GetHeaders()
 
 	int index = 0;
 
-	*(unsigned short*)&headers[index] = this->FileHeader.Type; index += sizeof(unsigned short);
-	*(unsigned long*)&headers[index] = this->FileHeader.Size; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->FileHeader.Reserved; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->FileHeader.Offset; index += sizeof(unsigned long);
+	//memcpy() is not used due to compiler-specfic structure padding
+	*(unsigned short*)&headers[index] = FileHeader.Type;			index += sizeof(unsigned short);
+	*(unsigned long*)&headers[index] = FileHeader.Size;			index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = FileHeader.Reserved;			index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = FileHeader.Offset;			index += sizeof(unsigned long);
 
-	*(unsigned long*)&headers[index] = this->ImageHeader.Size; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.Width; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.Height; index += sizeof(unsigned long);
-	*(unsigned short*)&headers[index] = this->ImageHeader.Planes; index += sizeof(unsigned short);
-	*(unsigned short*)&headers[index] = this->ImageHeader.BitCount; index += sizeof(unsigned short);
-	*(unsigned long*)&headers[index] = this->ImageHeader.Compression; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.ImageSize; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.WidthPixelsPerMeter; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.HeightPixelsPerMeter; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.pixelsUsed; index += sizeof(unsigned long);
-	*(unsigned long*)&headers[index] = this->ImageHeader.Importantpixels; index += sizeof(unsigned long);
-
+	*(unsigned long*)&headers[index] = ImageHeader.Size;			index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.Width;			index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.Height;			index += sizeof(unsigned long);
+	*(unsigned short*)&headers[index] = ImageHeader.Planes;			index += sizeof(unsigned short);
+	*(unsigned short*)&headers[index] = ImageHeader.BitCount;		index += sizeof(unsigned short);
+	*(unsigned long*)&headers[index] = ImageHeader.Compression;		index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.ImageSize;		index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.WidthPixelsPerMeter;	index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.HeightPixelsPerMeter;	index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.pixelsUsed;		index += sizeof(unsigned long);
+	*(unsigned long*)&headers[index] = ImageHeader.Importantpixels;	
+	
 	return headers;
 }
 
